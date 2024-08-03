802226a4a0b033d814102d76b79031676fbd706b
NaYoung1017 <skdud5664@naver.com>
2024-08-03 21:23:36 +0900

    Update LobbyManager.cs

--- Assets/Lobby/Scripts/LobbyManager.cs
+++ Assets/Lobby/Scripts/LobbyManager.cs
@@ -97,7 +97,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IInRoomCallbacks
 
     #region 서버연결
     // 게임의 화면 해상도를 960x540으로 설정
-    void Awake() => Screen.SetResolution(960, 540, false);
+    void Awake() => Screen.SetResolution(1280, 720, false);
     void Update()
     {
         // 로비에 몇명있는지, 접속한 인원수
