7de97181b26ae01e3e2c1dfb84ab53276301389b
CHOULOKY <166694194+CHOULOKY@users.noreply.github.com>
2024-09-12 11:07:14 +0900

    Incomplete Bird
    2024.09.12

--- Assets/InGame/Scripts/Character/FSM/Bird States.cs
+++ Assets/InGame/Scripts/Character/FSM/Bird States.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Handler;
 
 namespace BirdStates
 {
@@ -34,6 +33,8 @@ namespace BirdStates
         [Header("Move")]
         private List<Node> pathToPlayer;
         private int currentPathIndex;
+        private float curRoutineTime;
+        private bool isFollowPath;
 
         public MoveState(Bird _monster) : base(_monster) { }
 
@@ -41,10 +42,9 @@ namespace BirdStates
         {
             // Component
             rigid = monster.GetComponent<Rigidbody2D>();
-
+            
             // Move
-            Test();
-            // CoroutineHandler.StartCoroutine(Move(2f));
+            Move();
         }
 
         public override void OnStateUpdate()
@@ -54,10 +54,24 @@ namespace BirdStates
                 rigid = monster.GetComponent<Rigidbody2D>();
                 return;
             }
+            
+            curRoutineTime += Time.deltaTime;
+            if (curRoutineTime > 2f) {
+                curRoutineTime = 0;
+                Move();
+            }
+
+            if (isFollowPath) {
+                FollowPath();
+            }
         }
 
         public override void OnStateExit()
         {
+            // Move
+            curRoutineTime = 0;
+            isFollowPath = false;
+
             // Animator
             monster.RPCAnimFloat("xMove", 0);
             monster.RPCAnimFloat("yMove", 0);
@@ -86,28 +100,50 @@ namespace BirdStates
             }
         }
 
-        private IEnumerator Move(float _time)
+        private void Move()
         {
             RaycastHit2D hit = monster.CanSeePlayer();
             if (hit.collider != null) {
                 // 플레이어를 감지했을 때
                 Vector2Int start = new Vector2Int(Mathf.RoundToInt(monster.transform.position.x), Mathf.RoundToInt(monster.transform.position.y));
                 Vector2Int target = new Vector2Int(Mathf.RoundToInt(hit.collider.transform.position.x), Mathf.RoundToInt(hit.collider.transform.position.y));
-                
-                pathToPlayer = GameManager.Instance.astarManager.PathFinding(start, target, true, true); // 경로 계산
-                
+
+                // pathToPlayer = GameManager.Instance.astarManager.PathFinding(start, target, true, true);
+                pathToPlayer = GameObject.FindAnyObjectByType<AStarManager>().PathFinding(start, target, true, true); // 경로 계산
+
                 if (pathToPlayer != null && pathToPlayer.Count > 0) {
                     currentPathIndex = 0; // 경로 시작점으로 초기화
-                    CoroutineHandler.StartCoroutine(FollowPath()); // 경로를 따라 이동하는 코루틴 시작
+                    isFollowPath = true; // 경로를 따라 이동하는 루틴 시작
+                }
+                else {
+                    isFollowPath = false;
                 }
             }
-
-            yield return new WaitForSeconds(_time);
-
-            CoroutineHandler.StartCoroutine(Move(_time));
         }
-        private IEnumerator FollowPath()
+        private void FollowPath()
         {
+            if (currentPathIndex < pathToPlayer.Count) {
+                Vector2 targetPosition = new Vector2(pathToPlayer[currentPathIndex].x, pathToPlayer[currentPathIndex].y);
+
+                float dirX = rigid.position.x < targetPosition.x ? 1 : -1;
+                ControlFlip(dirX);
+                monster.RPCAnimFloat("xMove", Mathf.Abs(dirX));
+                monster.RPCAnimFloat("yMove", (float)targetPosition.y);
+
+                // 현재 위치와 타겟 노드 사이의 거리가 작으면 다음 노드로 이동
+                if (Vector2.Distance(monster.transform.position, targetPosition) > 0.1f) {
+                    monster.transform.position = Vector2.MoveTowards(monster.transform.position, targetPosition, Time.deltaTime * monster.status.moveSpeed);
+                    // yield return null; // 다음 프레임까지 대기
+                }
+                else {
+                    currentPathIndex++; // 다음 노드로 이동
+                }
+            }
+            else {
+                isFollowPath = false;
+            }
+
+            /*
             while (currentPathIndex < pathToPlayer.Count) {
                 Vector2 targetPosition = new Vector2(pathToPlayer[currentPathIndex].x, pathToPlayer[currentPathIndex].y);
                 // targetPosition = new Vector2(rigid.transform.rotation.eulerAngles.y == 180 ? -targetPosition.x : targetPosition.x, targetPosition.y);
@@ -118,11 +154,12 @@ namespace BirdStates
                 // 현재 위치와 타겟 노드 사이의 거리가 작으면 다음 노드로 이동
                 while (Vector2.Distance(monster.transform.position, targetPosition) > 0.1f) {
                     monster.transform.position = Vector2.MoveTowards(monster.transform.position, targetPosition, Time.deltaTime * monster.status.moveSpeed);
-                    yield return null; // 다음 프레임까지 대기
+                    // yield return null; // 다음 프레임까지 대기
                 }
 
                 currentPathIndex++; // 다음 노드로 이동
             }
+            */
         }
     }
 
--- Assets/InGame/Scripts/Manager/AStarManager.cs
+++ Assets/InGame/Scripts/Manager/AStarManager.cs
@@ -37,8 +37,8 @@ public class AStarManager : MonoBehaviour
         dontCrossCorner = _dontCrossCorner;
         
         // NodeArray의 크기 정해주고, isWall, x, y 대입
-        bottomLeft = new Vector2Int(Mathf.Min(_start.x - 1, _target.x - 1), Mathf.Min(_start.y - 1, _target.y - 1));
-        topRight = new Vector2Int(Mathf.Max(_start.x + 1, _target.x + 1), Mathf.Max(_start.y + 1, _target.y + 1));
+        bottomLeft = new Vector2Int(Mathf.Min(_start.x - 3, _target.x - 3), Mathf.Min(_start.y - 3, _target.y - 3));
+        topRight = new Vector2Int(Mathf.Max(_start.x + 3, _target.x + 3), Mathf.Max(_start.y + 3, _target.y + 3));
         sizeX = topRight.x - bottomLeft.x + 1;
         sizeY = topRight.y - bottomLeft.y + 1;
         NodeArray = new Node[sizeX, sizeY];
