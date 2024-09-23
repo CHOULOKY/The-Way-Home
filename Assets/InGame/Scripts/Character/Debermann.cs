43abbd461f6f8abd295b8fcfb4ede8d31b218169
CHOULOKY <dlsgml5131@gmail.com>
2024-09-23 19:34:41 +0900

    fix: Dobermann
    2024.09.23

--- Assets/InGame/Scripts/Character/Debermann.cs
+++ Assets/InGame/Scripts/Character/Debermann.cs
@@ -247,14 +247,24 @@ public class Dobermann : Monster, IPunObservable
             Gizmos.DrawWireCube(transform.position + Vector3.left * searchDistance, searchBox);
         else
             Gizmos.DrawWireCube(transform.position + Vector3.right * searchDistance, searchBox);
+        */
 
-
+        /*
         // Check attack box
         Gizmos.color = Color.green;
         if (transform.rotation.eulerAngles.y == 180)
             Gizmos.DrawWireCube(transform.position + Vector3.left * attackDistance, attackBox);
         else
             Gizmos.DrawWireCube(transform.position + Vector3.right * attackDistance, attackBox);
+        */
+
+        /*
+        // Wall Check Ray
+        Gizmos.color = Color.green;
+        if (transform.rotation.eulerAngles.y == 180)
+            Gizmos.DrawRay(transform.position, Vector3.left * 0.5f + Vector3.down * 0.7f);
+        else
+            Gizmos.DrawRay(transform.position, Vector3.right * 0.5f + Vector3.down * 0.7f);
         */
     }
 }
